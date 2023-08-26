bb1188bc3bfa647725f576c2f9cdc521e76580df
Amith-Jagannath <amithjagannath02@gmail.com>
2023-08-26 20:30:25 +0530

    Styles config

--- app/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-const SignUpPage = () => (
-  <div className="flex justify-center h-screen items-center">
-    <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-  </div>
-);
-
-export default SignUpPage;
