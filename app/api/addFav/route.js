4f1dd99f26a49c5e1767153e8c3a92217d2e173b
Amith-Jagannath <amithjagannath02@gmail.com>
2023-09-17 20:05:07 +0530

    vercel config

--- app/api/addFav/route.js
+++ app/api/addFav/route.js
@@ -4,26 +4,26 @@ import connectMongo from "../../../lib/mongodb";
 export async function POST(req) {
   console.log(req);
   const { user, songname, index } = await req.json();
-  console.log("inside");
-  console.log("User:", user);
-  console.log("Songname:", songname);
+  // console.log("inside");
+  // console.log("User:", user);
+  // console.log("Songname:", songname);
 
-  // console.log(await FavSong.find({}));
-  try {
-    await connectMongo();
-    const favSongExists = await FavSong.find({ user: user, song: songname });
-    if (favSongExists.length == 1) return;
-    let favSong = new FavSong({
-      user: user,
-      song: songname,
-      index: index,
-    });
-    console.log(favSong);
+  // // console.log(await FavSong.find({}));
+  // try {
+  //   await connectMongo();
+  //   const favSongExists = await FavSong.find({ user: user, song: songname });
+  //   if (favSongExists.length == 1) return;
+  //   let favSong = new FavSong({
+  //     user: user,
+  //     song: songname,
+  //     index: index,
+  //   });
+  //   console.log(favSong);
 
-    console.log(await favSong.save());
-    console.log("Successfull");
-  } catch (error) {
-    console.log(error);
-  }
+  //   console.log(await favSong.save());
+  //   console.log("Successfull");
+  // } catch (error) {
+  //   console.log(error);
+  // }
   return NextResponse.json({ message: "Added" }, { status: 200 });
 }
